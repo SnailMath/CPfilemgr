f632a9fd39ebc03292a973c1e4827b353eb061ba
SnailMath <snailmathprog@gmail.com>
2021-04-30 01:08:00 +0200

    Add support to view files

--- main.cpp
+++ main.cpp
@@ -7,18 +7,35 @@
 #include <sdk/os/mem.hpp>
 #include <sdk/os/input.hpp>
 
-/*
- * Fill this section in with some information about your app.
- * All fields are optional - so if you don't need one, take it out.
- */
 APP_NAME("Filemgr")
 APP_DESCRIPTION("A simple file manager")
 APP_AUTHOR("SnailMath")
 APP_VERSION("1.0.0")
 
+typedef unsigned int size_t;
+int abs(int a);
+void* realloc_sized(void* ptr, size_t oldsz, size_t newsz);
+int  stbi_my_read(void *user,char *data,int size); 
+void stbi_my_skip(void *user,int n);
+int  stbi_my_eof (void *user);
+
+#define STBI_MALLOC(sz)		malloc(sz)
+#define STBI_FREE(p)		free(p)
+#define STBI_REALLOC_SIZED(p,oldsz,newsz) realloc_sized(p,oldsz,newsz)
+#define STBI_NO_STDIO
+#define STBI_NO_LINEAR
+#define STBI_NO_HDR
+#define STBI_ASSERT
+#define STBI_NO_FAILURE_STRINGS
+//#define STBI_ONLY_JPEG
+//#define STBI_ONLY_PNG
+#define STB_IMAGE_IMPLEMENTATION
+#include "calc_image.h"
+
 #define EXIT_PROGRAM	-1
 #define GO_ROOT		-2
 #define GO_SD		-3
+#define GO_UP		-4
 
 int getCommandInput();
 
@@ -27,13 +44,22 @@ struct dirEntry{
 	char type;
 };
 const int maxFiles = 41;
+const int maxFilesDisplay = 41;
 int dirFiles = 0;
 struct dirEntry directory[64];
 
 char g_path[400];
 wchar_t g_wpath[400];
 
+void fileClick(char* fileName);
+void showImage(char* fileName);
+void show565(char* fileName);
+void showText(char* fileName);
+void showHex(char* fileName);
 
+
+#define to_ch(x) numToAscii[addr[x]]
+unsigned char numToAscii[257]= "................................ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~.................................................................................................................................";
 
 extern "C"
 void main() {
@@ -61,7 +87,7 @@ void main() {
 	
 		LCD_ClearScreen();
 		Debug_Printf(0,0,false,0,"%s",g_path);
-		Debug_Printf(0,1,false,0,"99 D .. (go up one directory)");
+		Debug_Printf(0,1,false,0,".. D (go up one directory)");
 		LCD_Refresh();
 	
 		int dirFiles = 0;
@@ -82,8 +108,12 @@ void main() {
 			//copy file type
 			thisfile.type=findInfoBuf.type==findInfoBuf.EntryTypeDirectory?'D':'F';
 			//display this
-			Debug_Printf(0,dirFiles+2,false,0,"%2d %c %s",dirFiles,thisfile.type,thisfile.fileName);
-			LCD_Refresh();
+			if(dirFiles<maxFilesDisplay){
+				Debug_Printf(0,dirFiles+2,false,0,"%2d %c %s",dirFiles,thisfile.type,thisfile.fileName);
+				LCD_Refresh();
+			}
+			//Debug_Printf(15,43,false,0,"%2d %s",dirFiles,thisfile.fileName);
+			//LCD_Refresh();
 			//save this dirEntry to directory
 			directory[dirFiles++] = thisfile;
 			
@@ -93,16 +123,22 @@ void main() {
 		findClose(findHandle);
 	
 		int go = getCommandInput();
-		//Debug_Printf(10,39,true,0,"go=%2x",go); 
+
 		if (go>=0 && go<dirFiles){
-			//Debug_Printf(0,0,true,0,"entering nr.%d!",go);
 			struct dirEntry thisfile = directory[go];
-			if(thisfile.type=='D'){
+			if(thisfile.type=='D'){ //type = folder
 				strcat(g_path,thisfile.fileName);
 				strcat(g_path,"\\");
+			}else{ //type = file
+				char thisFileName[400];
+				memset(thisFileName,0,sizeof(thisFileName));
+				strcat(thisFileName,g_path);
+				strcat(thisFileName,thisfile.fileName);
+				//Debug_Printf(0,0,true,0,"open:'%s'",thisFileName);LCD_Refresh();Debug_WaitKey();
+				fileClick(thisFileName);
 			}
 		}
-		if (go==99){
+		if (go==GO_UP){
 			//Debug_Printf(0,0,true,0,"entering ..!");
 			//seek from the back and remove the last two backslashs
 			int i=399;
@@ -145,9 +181,9 @@ void main() {
 }
 	
 int getCommandInput(){
-	char cmd[5]; //the command line
+	char cmd[13]; //the command line
 	int pos = 1; //the position of the cursor in cmd
-	strcpy (cmd,">_  ");
+	strcpy (cmd,">_          ");
 
 	struct InputEvent event;
 	while (true){
@@ -156,7 +192,7 @@ int getCommandInput(){
 		GetInput(&event,0xFFFFFFFF,0x10);
 		if(event.type==EVENT_KEY){
 			if(event.data.key.direction==KEY_PRESSED){
-				if(pos<=2){//If we have have less than 3 chars ('>' + 2 digits)
+				if(pos<=10){//If we have have less than 11 chars ('>' + 10 digits)
 					switch (event.data.key.keyCode){
 						case KEYCODE_0: cmd[pos++]='0';cmd[pos]='_';break;
 						case KEYCODE_1: cmd[pos++]='1';cmd[pos]='_';break;
@@ -171,7 +207,7 @@ int getCommandInput(){
 						case KEYCODE_DOT: 
 							if (pos==1){
 								if(cmd[1]=='.'){
-									return 99;
+									return GO_UP;
 								}else{
 									cmd[1]='.';
 								}
@@ -179,7 +215,7 @@ int getCommandInput(){
 						case KEYCODE_KEYBOARD: return GO_ROOT;
 						case KEYCODE_SHIFT: return GO_SD;
 					}
-				}//pos!<=2
+				}//pos!<=8
 				if (pos>1){ //If there are chars you can delete one.
 					if(event.data.key.keyCode==KEYCODE_BACKSPACE){
 						cmd[pos--]=' ';cmd[pos]='_';
@@ -189,11 +225,13 @@ int getCommandInput(){
 					return EXIT_PROGRAM;
 				}//!Clear
 				if(event.data.key.keyCode==KEYCODE_EXE){
-					if(cmd[1]!='_'&&cmd[1]!='.'){
+					while(cmd[1]!='_'&&cmd[1]!='.'){
 						int ret = cmd[1]-'0';
-						if(cmd[2]!='_'){
-							ret = (ret*10) + (cmd[2]-'0');
-						}//cmd[2]!='_'
+						int i=2;
+						while(cmd[i]!='_'){
+							ret = (ret*10) + (cmd[i]-'0');
+							i++;
+						}//cmd[i]!='_'
 						return ret;
 					}//cmd[1]!='_'
 				}//!EXE
@@ -210,3 +248,212 @@ int getCommandInput(){
 		}//EVENT_TOUCH
 	}//while (true)
 }
+
+int abs(int a){
+	return a<0?-a:a;
+}
+void* realloc_sized(void* oldptr, size_t oldsz, size_t newsz){
+	void* newptr = malloc(newsz);
+	memcpy(newptr,oldptr,oldsz);
+	free(oldptr);
+	return newptr;
+}
+static stbi_io_callbacks stbi__my_callbacks = {
+	stbi_my_read,
+	stbi_my_skip,
+	stbi_my_eof,
+};
+int  stbi_my_read(void *user,char *data,int size){
+	int ret = (int) read((int)user, data, size);
+	return ret;
+}
+void stbi_my_skip(void *user,int n){
+	lseek((int) user, n, SEEK_CUR);
+}
+int  stbi_my_eof (void *user){
+return 0;
+}
+
+//Open a file and display it when it was clicked.
+void fileClick(char* fileName){
+	LCD_ClearScreen();
+	Debug_Printf(3,2,false,0,"Open file '%s'",fileName);
+	Debug_Printf(3,3,false,0,"1 - as binary file");
+	Debug_Printf(3,4,false,0,"2 - as text file");
+	Debug_Printf(3,5,false,0,"3 - as image file (png/jpg/gif)");
+	Debug_Printf(3,6,false,0,"4 - as image file (.565)");
+	LCD_Refresh();
+
+	int selection = getCommandInput();
+	if       (selection==1){
+		showHex(fileName);
+	}else if (selection==2){
+		showText(fileName);
+	}else if (selection==3){
+		showImage(fileName);
+	}else if (selection==4){
+		show565(fileName);
+	}
+}
+void showImage(char* fileName){
+	uint16_t* VRAM = LCD_GetVRAMAddress();
+	int W,H; LCD_GetSize(&W,&H);
+	int w=0;
+	int h=0;
+	int c;
+	int fp;
+	for(long i=0;i<W*H;i++)VRAM[i]=0;
+	Debug_Printf(20,43,true,0,"Please Wait...");
+	LCD_Refresh();
+	for(long i=W*(H-12);i<W*H;i++)VRAM[i]=0;
+	//fp = open("\\fls0\\finn.png", OPEN_READ);
+	fp = open(fileName, OPEN_READ);
+	stbi__context s;
+	stbi__start_callbacks(&s, &stbi__my_callbacks, (void *) fp);
+	unsigned char *img = stbi__load_and_postprocess_8bit(&s, &w, &h, &c, 3);
+	close(fp);
+
+	uint32_t pos = 0;
+	for(int y=h-1;y>=0;y--){
+		for(int x=0;x<w;x++){
+			unsigned char r = img[pos++];
+			unsigned char g = img[pos++];
+			unsigned char b = img[pos++];
+			VRAM[x+y*W]=((r<<8)&0b1111100000000000) | ((g<<3)&0b0000011111100000) | ((b>>3)&0b0000000000011111);
+		}
+	}
+	LCD_Refresh();
+	if(w>0)
+		Debug_WaitKey();
+	stbi_image_free(img);
+}
+
+void showHex(char* fileName){
+	int fp;
+	uint8_t* addr;
+	fp = open(fileName, OPEN_READ);
+	getAddr(fp,0,(const void**)&addr);
+	close(fp);
+
+	LCD_ClearScreen();
+	Debug_Printf(0,0,false,0,"Viewing file %s",fileName);
+	/*int i=0;
+	for(int y=1;y<44;y++){
+		Debug_Printf(1,y,false,0,"%02X",i>>4);
+		for(int x=0;x<8;x++){
+			Debug_Printf(4+(x*5),y,false,0,"%02X%02X",(unsigned char)addr[i++],(unsigned char)addr[i++]);
+		}
+
+	}*/
+	Debug_Printf(0,4,false, 0,"   0  1  2  3   4  5  6  7");
+	for(int i=0; i<16;i++){
+		Debug_Printf(1,5+(2*i),false, 0,"%X %02X %02X %02X %02X  %02X %02X %02X %02X  %c%c%c%c %c%c%c%c", i,
+			addr [0+(16*i)],addr [1+(16*i)],addr [ 2+(16*i)],addr [ 3+(16*i)],addr [ 4+(16*i)],addr [ 5+(16*i)],addr [ 6+(16*i)],addr [ 7+(16*i)],
+			to_ch(0+(16*i)),to_ch(1+(16*i)),to_ch( 2+(16*i)),to_ch( 3+(16*i)),to_ch( 4+(16*i)),to_ch( 5+(16*i)),to_ch( 6+(16*i)),to_ch( 7+(16*i)));
+		Debug_Printf(1,6+(2*i),false, 0, "  %02X %02X %02X %02X  %02X %02X %02X %02X  %c%c%c%c %c%c%c%c",
+			addr [8+(16*i)],addr [9+(16*i)],addr [10+(16*i)],addr [11+(16*i)],addr [12+(16*i)],addr [13+(16*i)],addr [14+(16*i)],addr [15+(16*i)],
+			to_ch(8+(16*i)),to_ch(9+(16*i)),to_ch(10+(16*i)),to_ch(11+(16*i)),to_ch(12+(16*i)),to_ch(13+(16*i)),to_ch(14+(16*i)),to_ch(15+(16*i)));
+	}
+
+	LCD_Refresh();
+	Debug_WaitKey();
+}
+
+//Debug_Printf(0,4,false, 0,"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ"); //44 lines, 53 chars each
+void showText(char* fileName){
+	int fp;
+	uint8_t* addr;
+	fp = open(fileName, OPEN_READ);
+	getAddr(fp,0,(const void**)&addr);
+	close(fp);
+
+	uint32_t page = 0; //the currently displeyed page
+	int32_t skip = 1; //number of frames that should be advanced
+	uint32_t i = 0;
+	bool end = false;
+	while(true){
+		LCD_ClearScreen();
+		Debug_Printf(0,0,false,0,"Viewing file %s",fileName);
+		while(skip>0){
+			int      j = 0;
+			char line[54];
+			for(int y=1;y<43;y++){
+				j=0;
+				while(true){
+					unsigned char ch = addr[i+j];
+					if(ch==0){
+						end = true;
+						line[j]=0;
+						break;
+					}
+					if(ch=='\n'||ch=='\r'){
+						line[j]=0;
+						j++;
+						break;
+					}
+					if(j==53){
+						line[j]=0;
+						int j2 = j;
+						while(j2>0){
+							if(line[j2]==' '){
+								line[j2]=0;
+								j=j2+1;
+								break;
+							}
+							j2--;
+						}
+						break;
+					}
+					line[j]=numToAscii[(int)ch];
+					j++;
+				}
+				if(skip==1) //don't print pages if you skip them
+					Debug_Printf(0,y,false,0,"%s",line);
+				i+=j;
+				if(end){
+					break;
+				}
+			}
+			skip--; //turning pages done
+			page++; //we are now on the next page.
+			if(end){
+				break;
+			}
+		}
+		Debug_Printf(13,43,false,0,"To skip x pages type x+100 (page %d)",page);
+		LCD_Refresh();
+		skip = getCommandInput();
+		if(skip==EXIT_PROGRAM||end)
+			break;
+		skip-=100;
+		if(skip<=0)
+			skip=1;
+	}
+}
+
+void show565(char* fileName){
+	uint16_t* VRAM = LCD_GetVRAMAddress();
+	int W,H; LCD_GetSize(&W,&H);
+
+	int fp;
+	uint16_t* addr;
+	fp = open(fileName, OPEN_READ);
+	getAddr(fp,0,(const void**)&addr);
+	close(fp);
+
+	Debug_Printf(0,42,false,0,"Please type in the width (usually 160)");
+	//the getCommandInput subroutine will call LCD_Refresh, so I don't have to do so here...
+	long width = getCommandInput();
+	if(width<1)
+		width=160;
+	uint32_t pos = 0;
+	for(int y=0;y<528;y++){
+		for(int x=0;x<width;x++){
+			uint16_t color= addr[pos++];
+			VRAM[x+y*W]=color;
+		}
+	}
+	LCD_Refresh();
+	Debug_WaitKey();
+}
+
