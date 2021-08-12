9a0324db4405edcc5f41ec94d5210fcf0e71c7c4
SnailMath <snailmathprog@gmail.com>
2021-08-12 15:02:48 +0200

    Remove pnm

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ int  stbi_my_eof (void *user);
 #define STBI_NO_FAILURE_STRINGS
 //#define STBI_ONLY_JPEG
 //#define STBI_ONLY_PNG
+#define STBI_NO_PNM
 #define STB_IMAGE_IMPLEMENTATION
 #include "calc_image.h"
 
